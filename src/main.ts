a47b5c1eb17830dbe9e369cfc9b403cd27e6eb65
George Maged <geo.morice@gmail.com>
2021-02-06 22:25:55 +0200

    🚀 Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import get from 'lodash/get'
+import yaml from 'js-yaml'
+import fs from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const file_path: string = core.getInput('path')
+    const file = yaml.load(fs.readFileSync(file_path, 'utf-8'))
+    console.log(file)
   } catch (error) {
     core.setFailed(error.message)
   }
